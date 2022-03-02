abc9bd14989d2257763e12a9f99adfb8c0b0f9dc
tikayzsila <kwetrus@yandex.ru>
2022-03-02 17:32:56 +0300

    app template

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,31 @@
+class QuestionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+    )
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    @questions = Question.all
+  end
+end
